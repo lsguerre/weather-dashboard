faba84f4acbe0047345ae73822d968fb3063bbe3
Opal <guerre5lily@gmail.com>
2022-03-30 17:44:20 -0700

    daily weather appended to page for city

---
+++ README.md
@@ -0,0 +1,3 @@
+# Weather Dashboard
+
+* user can enter the city and a summary of the weather for that city will be displayed on the page
---
+++ js/index.js
@@ -0,0 +1,68 @@
+var cityFormEl = document.querySelector('#city-form')
+var cityInput = document.querySelector('#citySearch')
+var searchBtn = document.querySelector('#search-btn')
+var resultsContainerEl = document.querySelector('#currentForecast')
+
+const apiKey = 'c00dbed7550689a0253675d600f7c3a6'
+
+var searchHandler = function(event) {
+    event.preventDefault()
+
+    var city = cityInput.value.trim()
+
+    if(city) {
+        getWeather(city)
+
+        resultsContainerEl.textContent = ''
+        cityInput.value = ''
+
+    } else {
+        alert('Please enter a city')
+    }
+    console.log(event)
+}
+
+var getWeather = function(city) {
+    var city = cityInput.value.trim()
+    var currentForecast = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + apiKey
+
+
+
+
+    fetch(currentForecast)
+        .then(function(response) {
+            if (response.ok) {
+                response.json()
+                    .then(function(data) { 
+                    displayWeather(data, city)
+                    console.log(data)
+                })
+            }
+        })
+        .catch(function(error) {
+            
+            alert('Unable to connect to open weather')
+        })
+        
+
+
+}
+
+var displayWeather = function(currentForecast) {
+
+    var cityWeather = $(`
+            <h2 id="currentCity">
+                ${currentForecast.name} 
+            </h2>
+            <p>Temperature: ${currentForecast.main.temp} °F</p>
+            <p>Humidity: ${currentForecast.main.humidity}\%</p>
+            <p>Wind Speed: ${currentForecast.wind.speed} MPH</p>
+           
+        `)
+        $('#currentForecast').append(cityWeather)
+}
+
+
+// cityFormEl.addEventListener('submit', searchHandler)
+
+$('#search-btn').click('submit', searchHandler)
